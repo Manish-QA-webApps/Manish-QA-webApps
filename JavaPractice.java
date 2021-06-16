b191f4708aabe6ce9d02beb448e1b32e38c0986e
Manish-QA-webApps <64822617+Manish-QA-webApps@users.noreply.github.com>
2021-06-16 19:26:18 +0530

    Add files via upload

---
+++ JavaPractice.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaPractice {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		
+		int A = 10;
+		int B = 20;
+		int C;
+		
+		C= A-B;
+		
+		System.out.println(C);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
